fecb40740831822280e5e9bf7b798f6a8ae73c1c
burningman2k20 <burningman2k9@gmail.com>
2020-09-22 15:29:15 -0400

    sample page with header and footer

---
+++ vara-website/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Header() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p> example header file </p>{" "}
+			</header>{" "}
+		</div>
+	);
+}
+
+export default Header;
--- vara-website/src/index.js
+++ vara-website/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Header from "./Header";
+import Footer from "./Footer";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Header />
+	</React.StrictMode>,
+	document.getElementById("header"),
+);
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
+);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Footer />
+	</React.StrictMode>,
+	document.getElementById("footer"),
 );
 
 // If you want your app to work offline and load faster, you can change
