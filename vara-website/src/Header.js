ebd2a586ef4c59e1e53206fac96ebb26716d9ad3
burningman2k20 <burningman2k9@gmail.com>
2020-09-24 13:37:27 -0400

    Update Header.js

--- vara-website/src/Header.js
+++ vara-website/src/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
-import VaraLogo from "./Images/vara_logo.png";
+import VaraCar from "./Images/vara_car.png";
 import VaraName from "./Images/vara_name.png";
 import "./App.css";
 
 function Header() {
     return (
 		<div className = "App" >
+			<img src = { VaraCar} alt = ""/>
 			<img src = { VaraName } alt = ""/>
 		</div>
     );
