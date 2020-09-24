37fd77b0bf883741d722429d3b755667396370e6
mojfazel <fazeli.mojtaba@gmail.com>
2020-09-24 18:00:59 -0400

    udpates sign pages

--- vara-website/src/Components/SignUp.js
+++ vara-website/src/Components/SignUp.js
@@ -4,30 +4,30 @@ import '../Not_Used/App.css';
 
 class SingUp extends Component {
     singUp = (e) => {
-	console.log(e.target.toString());
+        e.preventDefault();
+	    console.log(e.target);
 };
     render() {
         return (
             <div class="d-flex justify-content-center">
-                <form class="col-7 d-flex flex-column" onSubmit={this.singUp}>
-                     <label class="d-flex justify-content m-0 mb-4">
-                        <h5 class="d-flex flex-column justify-content-center col-5 text-right m-0">Name:</h5>
-                        <input class="col-7 border rounded p-2" type="text" name="name" placeholder="Name" />
-                    </label>
+                <form class="col-4 d-flex flex-column" onSubmit={this.singIn}>
+					<label class="d-flex justify-content-center m-0 mb-4">
+						<h5 class="d-flex flex-column justify-content-center col-4 text-right m-0">Name:</h5>
+						<input class="col-8 border rounded p-2" type="text" name="name" placeholder="Name" />
+					</label>
 
-                    <label class="d-flex justify-content m-0 mb-4">
-                        <h5 class="d-flex flex-column justify-content-center col-5 text-right m-0">Email:</h5>
-                        <input class="col-7 border rounded p-2" type="email" name="email" placeholder="Email" />
-                    </label>
-
+					<label class="d-flex justify-content-center m-0 mb-4">
+						<h5 class="d-flex flex-column justify-content-center col-4 text-right">Email:</h5>
+						<input class="col-8 border rounded p-2" type="email" name="email" placeholder="Email"/>
+					</label>
                     <label class="d-flex justify-content-center mb-4">
-                        <h5 class="d-flex flex-column justify-content-center col-5 text-right">Password:</h5>
-                        <input class="col-7 border rounded p-2" type="password" name="password" placeholder="Password" />
+                        <h5 class="d-flex flex-column justify-content-center col-4 text-right">Password:</h5>
+                        <input class="col-8 border rounded p-2" type="password" name="password" placeholder="Password" />
                     </label>
                 
                     <label class="d-flex justify-content-center mb-4">
-                        <h5 class="d-flex flex-column justify-content-center col-5 text-right">Confirm Password:</h5>
-                        <input class="col-7 border rounded p-2" type="password" name="password" placeholder="Password" />
+                        <h5 class="d-flex flex-column justify-content-center col-4 text-right">Re-type Password:</h5>
+                        <input class="col-8 border rounded p-2" type="password" name="password" placeholder="Password" />
                     </label>
                     <button class="btn btn-primary col-2 ml-auto" type="submit">Sign up</button>
                 </form>
