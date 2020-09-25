eaed23a86796855e4c291cd7a2a5ef13897a53f6
RoaaAlmadi <roaa.almadi@gmail.com>
2020-09-24 22:13:25 -0700

    fixed the header and sstarted with the footer

--- vara-website/src/Components/Header/Header.js
+++ vara-website/src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import VaraLogo from "../../Images/vara_logo.png";
 import VaraName from "./../../Images/vara_name.png";
+import VaraTagLine from "./../../Images/vara_tagline.png";
 import "../../Not_Used/App.css";
 
 function Header() {
     return (
-		<div className = "App" >
-			<img src = { VaraName } alt = ""/>
+		<div className = "App color_black inline-block" >
+			<img className="logo" src = { VaraName } alt = ""/>
+			<img className="logo" src = { VaraTagLine } alt = ""/>
 		</div>
     );
 }
--- vara-website/src/Components/Main.js
+++ vara-website/src/Components/Main.js
@@ -5,7 +5,8 @@ import { Nav } from "react-bootstrap";
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
-import VaraName from "../Images/vara_name.png";
+import SignUp from "./SighUp";
+import VaraCar from "../Images/vara_car.png";
 
 class Main extends Component {
 
@@ -13,15 +14,22 @@ class Main extends Component {
         return (
 			<HashRouter>
             <div className="content">
+				
 				<ul className = "Header">
-					<li><NavLink exact to = "/">Home</NavLink></li>
-					<li><NavLink to = "/Stuff">Stuff</NavLink></li>
-					<li><NavLink to = "/Contact">Contact</NavLink></li>
+					<div className="menu-items">
+						<img className="nav-logo" src = { VaraCar } alt = ""/>
+						<li><NavLink exact to = "/">Home</NavLink></li>
+						<li><NavLink to = "/Stuff">Stuff</NavLink></li>
+						<li><NavLink to = "/Contact">Contact</NavLink></li>
+						<botton className="signup-btn"><NavLink to = "/SignUp">Sing Up</NavLink></botton>
+					</div>
 				 </ul>
 				<div className = "content" >
-            		<Route exact path = "/" component = { Home }/>
+            		<Route exact path = "/Home" component = { Home }/>
 					<Route exact path = "/Stuff" component = { Stuff }/>
 					<Route exact path = "/Contact" component = { Contact }/>
+            		<Route exact path = "/SingUp" component = { SignUp }/>
+
 				</div>
 			</div>
 			</HashRouter>
